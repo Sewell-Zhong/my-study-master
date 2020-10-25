d714ab54c90dfe4a1185e4b4e3715ff03a4fa307
Mr_Z <zhongsen94@163.com>
2020-10-26 00:14:59 +0800

    红黑树

---
+++ data-structure-master/src/main/java/com/_2_tree/_4_RBTree/code/printer/Printer.java
@@ -0,0 +1,32 @@
+package com._2_tree._4_RBTree.code.printer;
+
+public abstract class Printer {	
+	/**
+	 * 二叉树的基本信息
+	 */
+	protected BinaryTreeInfo tree;
+	
+	public Printer(BinaryTreeInfo tree) {
+		this.tree = tree;
+	}
+	
+	/**
+	 * 生成打印的字符串
+	 */
+	public abstract String printString();
+	
+	/**
+	 * 打印后换行
+	 */
+	public void println() {
+		print();
+		System.out.println();
+	}
+	
+	/**
+	 * 打印
+	 */
+	public void print() {
+		System.out.print(printString());
+	}
+}
